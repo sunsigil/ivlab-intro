cca717504649bbd51c5524c661356071b0fe8b6d
sunsigil <45524115+sunsigil@users.noreply.github.com>
2022-12-03 02:33:05 -0600

    Minor improvements to paintbrush QoL

--- Assets/Scripts/Player/PaintGun.cs
+++ Assets/Scripts/Player/PaintGun.cs
@@ -11,20 +11,53 @@ public class PaintGun : MonoBehaviour
 
     [SerializeField]
     Transform anchor;
+    [SerializeField]
+    LineRenderer line;
+    [SerializeField]
+    Transform mark;
+
+    Painting target;
+    Vector3 target_point;
 
     public void Fire()
+    {
+        if (target == null)
+        { return; }
+
+        target.Paint(target_point, radius, colour);
+
+        line.gameObject.SetActive(true);
+        line.SetPosition(0, anchor.position);
+        line.SetPosition(1, target_point);
+        line.startWidth = radius;
+        line.endWidth = radius * 2;
+    }
+
+    private void FixedUpdate()
     {
         RaycastHit hit;
         Ray ray = new Ray(anchor.position, Camera.main.transform.forward);
         Physics.SphereCast(ray, radius, out hit);
 
-        if(hit.transform == null)
-        { return; }
+        if (hit.transform == null)
+        {
+            target = null;
+            return;
+        }
 
-        Painting painting = hit.transform.GetComponent<Painting>();
-        if (painting == null)
-        { return; }
+        target = hit.transform.GetComponent<Painting>();
+        target_point = hit.point;
 
-        painting.Paint(hit.point, radius, colour);
+        if (target == null)
+        {
+            line.gameObject.SetActive(false);
+            mark.gameObject.SetActive(false);
+            return;
+        }
+
+        mark.gameObject.SetActive(true);
+        mark.transform.position = Vector3.Lerp(anchor.position, target_point, 0.95f);
+        mark.transform.LookAt(Camera.main.transform.position, Vector3.up);
+        mark.transform.localScale = new Vector3(radius * 2.5f, radius * 2.5f, 1);
     }
 }
